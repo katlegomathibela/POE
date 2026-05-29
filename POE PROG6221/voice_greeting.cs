f071135dcdbc0a4d77ac8d766a90e20fb3adf544
katlegomathibela <ST10486189@rcconnect.edu.za>
2026-05-29 18:37:21 +0200

    Added a new Project
    Created WPF project and configured application structure.
    Added voice greeting and startup audio functionality.
    Designed GUI interface with logo, chat screen and navigation.
    Improved chatbot responses, error handling and user experience.

---
+++ POE PROG6221/voice_greeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace POE
+{
+    public class voice_greeting
+    {
+        public void greet()
+        {
+            string auto_path = AppDomain.CurrentDomain.BaseDirectory;
+            string full_path = Path.Combine(auto_path, "greeting.wav");
+
+            SoundPlayer greetMe = new SoundPlayer(full_path);
+            greetMe.Play();
+        }
+    }
+}
